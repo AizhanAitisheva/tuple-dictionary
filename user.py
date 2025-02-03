860fdf05a96d6120cda2990a6800a77c5563657e
AizhanAitisheva <aitishevaaijan@gmail.com>
2025-02-03 22:03:52 +0600

    Update user.py

--- user.py
+++ user.py
@@ -1,1 +1,9 @@
+user = {}
+
+user["name"] = input("What is the user's name? ")
+user["age"] = input("What is the user's age? ")
+user["country"] = input("What is the user's country of birth? ")
+user["known_for"] = input("What is the user known for? ")
+
+
 
