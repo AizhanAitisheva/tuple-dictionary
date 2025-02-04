a77dbc779aa4926ba450c3cd45eef0684eba6ee1
AizhanAitisheva <aitishevaaijan@gmail.com>
2025-02-04 18:35:41 +0600

    Update myfamily.py

--- myfamily.py
+++ myfamily.py
@@ -9,13 +9,7 @@ print(myfamily[2])
 print(myfamily[4])
 
 #task3 check whether we can add an item by using the append() method
-try:
-    myfamily.append('me')
-except AttributeError as e:
-    print('Error:', e) #Tuples do not support append()
+#Tuples do not support append() because they are immutable
 
 #task4 check whether we can remove the item brother by using the pop() method
-try:
-    myfamily.pop()
-except AttributeError as e:
-    print('Error:', e) #Tuples do not support pop()
+#Tuples do not support pop() method
